6628d93b4834c4ee0df3111cc7181ed69802b92d
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2020-04-27 19:46:19 -0300

    feat: 🎸 Adiciona validaçao ao formulario

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
   ],
   declarations: [
     SigninComponent
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
-  constructor() { }
+  private loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-
+    this.loginForm = this
+      .formBuilder
+      .group({
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+      });
   }
 }
