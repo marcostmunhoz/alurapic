d8f42d93d3fa48f5bf4ae981aa823f3308c72ae0
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2020-04-27 21:38:37 -0300

    feat: 🎸 Adiciona componente vmessage para mensagens de erro

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { SigninComponent } from './signin/signin.component';
+import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    VmessageModule,
   ],
   declarations: [
     SigninComponent
---
+++ src/app/shared/components/vmessage/vmessage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ap-vmessage',
+  templateUrl: './vmessage.component.html',
+  styleUrls: ['./vmessage.component.css']
+})
+export class VmessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
